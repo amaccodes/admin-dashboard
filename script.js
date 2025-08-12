4894b30f3fd20026473ae9702dd18c6afc5a7ad3
Aaron McDonald <aaronmcd1025@gmail.com>
2025-08-11 23:29:38 -0400

    Polished the rest of the app. Finalized the structure and the UI/UX of the page.Completed the main content section of the dashboard.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ console.log("Hello World!");
 
 const projectNode = document.querySelector('#project-container')
 
-const div = document.createElement('div')
-
 for (i = 1; i <= 6; i++) {
     // create div element
     const div = document.createElement('div')
@@ -90,5 +88,59 @@ for (i = 1; i <= 6; i++) {
     _delete.alt = 'Delete Icon'
     _delete.height = 24
     parent2.appendChild(_delete)
+}
 
+
+// parent for the extra info section
+
+const extraParent = document.querySelector('#extra-info')
+
+// create div element
+const div = document.createElement('div')
+
+// style div element
+div.style.display = 'flex'
+div.setAttribute('id', 'announcements')
+div.style.flexDirection = 'column'
+div.style.borderRadius = '12px'
+div.style.alignItems = 'center'
+div.style.textAlign = 'center'
+div.style.border = '1px solid black'
+div.style.backgroundColor = 'white'
+div.style.padding = '24px'
+div.style.paddingTop = '0px'
+
+// append div to its designated parent node
+extraParent.appendChild(div)
+
+for (i = 1; i <= 3; i++) {
+    // create title and style it
+    const p = document.createElement('p')
+    p.style.margin = '0px'
+    p.style.padding = '0px'
+    p.style.fontSize = '14px'
+    p.style.fontWeight = 'bold'
+    p.style.marginTop = '24px'
+
+    // create description and style it
+    const describe = document.createElement('p')
+    describe.style.margin = '0px'
+    describe.style.padding = '0px'
+    describe.style.fontSize = '12px'
+    describe.style.paddingTop = '10px'
+
+    // dynamic logic to create the text content of the titles and descriptions
+    if (i == 1) {
+        p.textContent = 'Site Maintenance'
+        describe.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br><br>_____________________'
+    } else if (i == 2) {
+        p.textContent = 'Community Share Day'
+        describe.innerHTML = 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br><br>_____________________'
+    } else if (i == 3) {
+        p.textContent = 'Updated Privacy Policy'
+        describe.textContent = 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    } 
+    const parent = document.querySelector('#announcements')
+    parent.appendChild(p)
+    parent.appendChild(describe)
 }
